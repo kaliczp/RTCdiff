e7f8d325edf09fe1dfc0223cc73a77859ee9e150
Péter Kalicz <kaliczp@gmail.com>
2021-01-30 10:55:00 +0100

    Data processing wiht data adjustment
    Now tests.txt contains some data with adjustments. Now the calculation
    is modified to proces data with adjustments.

--- Readme.R
+++ Readme.R
@@ -4,16 +4,25 @@ vaisala <- strptime(substr(ttraw,27,48), format = "%b %d %T %Y", tz = "UTC")
 vaidate <- as.Date(vaisala)
 time <- as.POSIXct(paste(vaidate, tttime))
 kulonbs <- vaisala - time
+## Adjustments identification and date
+clkadjust <- kulonbs > -1
+## Exclude clock adjustment for aggregation
+vaidate[clkadjust] <- vaidate[clkadjust] + 1
 aggreg <- tapply(kulonbs, vaidate, mean)
-res.df <- data.frame(Readdiff = diff(as.Date(names(aggreg))),
+okvaidate <- as.Date(names(aggreg))
+nrnewadj <- which(diff(okvaidate) == 1) + 1
+okvaidate[nrnewadj] <- okvaidate[nrnewadj] - 1
+## Process dataframe differences from real differences
+res.df <- data.frame(Readdiff = diff(okvaidate),
                      Clockdiff = diff(aggreg)
                      )
+res.df <- res.df[res.df$Readdiff > 0, ]
 
 plot(res.df, xlab = "Readdiff [days]",
-     ylab = "clockdiff [mins]")
+     ylab = "clockdiff [secs]")
 res.lm <- lm(Clockdiff ~ Readdiff, res.df)
 abline(res.lm)
 ## Napi diff secben -2.54
-coef(res.lm)[2] * 60
+coef(res.lm)[2]
 ## Havi diff percben -1.27
-coef(res.lm)[2] * 30
+(coef(res.lm)[2] * 30) / 60
